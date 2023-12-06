65747bf58bd514c8ed0ca1cdf28c71bba133fafd
Sung Chaeyeon <ch.yeondev@gmail.com>
2023-12-07 02:29:44 +0900

    [level 2] Title: 택배상자, Time: 158.92 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,6 +1,17 @@
+import java.util.Stack;
+
 class Solution {
     public int solution(int[] order) {
-        int answer = 0;
-        return answer;
+        Stack<Integer> stack = new Stack<>();
+
+        int orderIndex = 0;
+        for(int box = 1; box <= order.length; box++) {
+            stack.push(box);
+            while(!stack.isEmpty() && stack.peek() == order[orderIndex]) {
+                stack.pop();
+                orderIndex++;
+            }
+        }
+        return orderIndex;
     }
 }
