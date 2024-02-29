386ae57a41fb9f476429bd9ec3efc03cc06a83b9
Sung Chaeyeon <ch.yeondev@gmail.com>
2024-02-29 22:36:52 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -7,6 +7,5 @@ with tmp as (
 
 select distinct id, email, first_name, last_name
 from developers as d
-    left join tmp on 1=1
-where d.skill_code & tmp.code = tmp.code
+    inner join tmp on d.skill_code & tmp.code
 order by id
