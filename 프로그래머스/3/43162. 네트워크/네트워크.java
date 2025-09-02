59233cc29b28eb87748aaa1d284c71549e9b85bf
깃짱 <107979804+gitchannn@users.noreply.github.com>
2025-09-02 13:32:31 +0900

    [level 3] Title: 네트워크, Time: 0.42 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,51 @@
+import java.util.*;
+
 class Solution {
-    private int answer = 0;
+    
+    private int[] groups;
+    
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[computers.length];
-        
-        for(int i=0; i<n; i++){
-            if(!visited[i]) {
-                dfs(i, computers, visited);
-                answer++;
-            }
+        groups = new int[n];
+        for(int i=0; i<n; i++) {
+            groups[i] = i;
         }
         
-        
-        return answer;
-    }
-    
-    private void dfs(int start, int[][] computers, boolean[] visited){
-        visited[start] = true;
-        
-        for(int i=0; i<computers.length; i++){
-            if(!visited[i]) {
-                if(computers[start][i] == 1 && !visited[i]) {
-                    dfs(i, computers,visited);
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                boolean connected = computers[i][j] == 1;
+                
+                if(connected) {
+                    // connect
+                    int par1 = findParent(i);
+                    int par2 = findParent(j);
+
+                    if(par1 != par2) {
+                        groups[par1] = par2;
+                    }
                 }
             }
         }
+        
+        for(int i=0; i<n; i++) {
+            groups[i] = findParent(groups[i]);
+        }
+        
+        Set<Integer> cnt = new HashSet<>();
+        for(int group: groups) {
+            cnt.add(group);
+        }
+        
+        return cnt.size();
+    }
+    
+    private int findParent(int com) {
+        if(groups[com] == com) {
+            return com;
+        }
+        
+        int parent = findParent(groups[com]);
+        groups[com] = parent;
+        
+        return parent;
     }
 }
